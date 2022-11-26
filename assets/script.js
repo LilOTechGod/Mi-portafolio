dd0b6040f930a9a8b72e3d3a6f530d78c6ff4da7
unknown <perezoscar360@gmail.com>
2022-11-25 18:30:54 -0600

    finished final draft of portfolio

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav-trigger');
+//     var instances = document.querySelectorAll('.Sidenav').init(elems, options);
+//   });
